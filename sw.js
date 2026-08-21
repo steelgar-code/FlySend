5ce9a3e57b42e0c6697a7afc484d034e81830ec0
steelgar-code <steelgar@gmail.com>
2026-08-21 23:30:24 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,38 @@
-const CACHE_NAME = 'flysend-v1';
+const CACHE_NAME = 'flysend-v2';
+
 const ASSETS = [
   './',
   './index.html',
-  './app.js',
   './manifest.json',
   './favicon.png',
-  './icon-192.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
+self.addEventListener('install', (event) => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
   );
 });
